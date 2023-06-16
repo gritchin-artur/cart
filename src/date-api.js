82d5b80a5f1162a1b82dc5f9747b2703d5ffb72c
gritchin-artur <123599698+gritchin-artur@users.noreply.github.com>
2023-06-16 16:18:24 +0200

    1

---
+++ src/date-api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+
+export async function fetchFindDate(date) {
+    const URL = 'http://numbersapi.com/'
+    try {
+        const response = await axios.get(
+            `${URL}${date}/type`
+        );
+        return response.data;
+  } catch (error) {
+    // Notify.failure("We're sorry, but you've reached the end of search results.")
+    }
+
+};
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { fetchFindDate } from './date-api';
+
+const input = document.querySelector('.enter-date');
+const result = document.querySelector('.result-of-serch');
+
+
+input.addEventListener
