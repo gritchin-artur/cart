f20e0be70d6e9ab45ae594fa8938a2afbae06b67
gritchin-artur <123599698+gritchin-artur@users.noreply.github.com>
2023-06-19 21:28:30 +0200

    Update date-api.js

--- src/date-api.js
+++ src/date-api.js
@@ -1,31 +1,20 @@
-// import axios from "axios";
+import axios from "axios";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
-
-// export async function fetchFindDate(date, type) {
-//     const URL = 'http://numbersapi.com/'
-//     try {
-//         const response = await axios.get(
-//             `${URL}${date}/${type}`
-//         );
-//         return response.data;
-//   } catch (error) {
-//     Notify.failure("We're sorry, but you've reached the end of search results.")
-//     }
-
-// };
 
 
 export async function fetchFindDate(date, type) {
     const URL = 'http://numbersapi.com/'
-    fetch(`${URL}${date}/${type}`)
-    .then((response) => response.text())
-.then((data) => {
-    console.log(data)
-return data;
-})
-    
+    try {
+        const response = await axios.get(
+            `${URL}${date}/${type}`
+        );
+        return response.data;
+  } catch (error) {
+    Notify.failure("We're sorry, but you've reached the end of search results.")
+    }
+
 };
+
 
 
 
