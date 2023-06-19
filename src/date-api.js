75c1eb8236185d0b6e48527f72179307bf6ac13a
gritchin-artur <123599698+gritchin-artur@users.noreply.github.com>
2023-06-19 19:51:00 +0200

    1

--- src/date-api.js
+++ src/date-api.js
@@ -2,15 +2,15 @@ import axios from "axios";
 // import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 
-export async function fetchFindDate(date) {
+export async function fetchFindDate(date, type) {
     const URL = 'http://numbersapi.com/'
     try {
         const response = await axios.get(
-            `${URL}${date}/type`
+            `${URL}${date}/${type}`
         );
         return response.data;
   } catch (error) {
-    // Notify.failure("We're sorry, but you've reached the end of search results.")
+    Notify.failure("We're sorry, but you've reached the end of search results.")
     }
 
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
 import { fetchFindDate } from './date-api';
 
 const input = document.querySelector('.enter-date');
-const result = document.querySelector('.result-of-serch');
+const card = document.querySelector('.cart-date')
+const typeSearch = document.querySelector('.type-select')
+
+let date = 0;
+let type = '';
+
+typeSearch.addEventListener('input', (event) => {
+    type = event.target.value;
+    console.log(date)
+    fetchFindDate(date, type)
+     .then(() => {
+         input.value = '';
+    })
+});
+
+input.addEventListener('input', (event) => {
+    date = event.target.value;
+    fetchFindDate(date, type)
+    .then((result) => {
+        cardText(result)
+    })
+        .catch((error) => {
+            alert('error')
+        })
+});
 
 
-input.addEventListener
+function cardText(result) {
+    console.log(result)
+    card.innerHTML =
+        ` <p class="result-of-serch">${result}</p>`
+}
